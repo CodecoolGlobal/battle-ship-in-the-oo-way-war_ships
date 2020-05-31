bbd467925c33f3c160bd29f0795b3f4fcdb2c15d
Dominik <dominikdrazek99@gmail.com>
2020-05-31 19:09:09 +0200

    Added basics of class Player

--- Battleships/Player.cs
+++ Battleships/Player.cs
@@ -2,11 +2,29 @@ using System;
 
 namespace BattleShips
 {
-    class Player
+    public class Player
     {
-        public Player()
-        {
 
+        private Ocean PlayerOcean;
+        private string Name;
+        private bool IsComp;
+        private bool IsLoser;
+
+        public Player(string name)
+        {
+            this.Name = name;
+            this.PlayerOcean = new Ocean(); 
         }
+
+        public int enemyAttack(int[] coords)
+        {
+        }
+        
+        public bool checkIfLoser()
+        {
+            foreach (var ship in Ship);
+        }
+
+
     }
 }
