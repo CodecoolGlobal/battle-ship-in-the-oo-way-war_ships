17e96d3901524752e22d9d60ea14094d013b152a
ndrd <debowyen@gmail.com>
2020-05-31 12:31:20 +0200

    Initail commit after merge with warup

---
+++ Battleships/Ocean.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace BattleShips
+{
+    public class Ocean
+    {
+        private static int HEIGHT = 10;
+        private static int WIDTH = 10;
+        private List<List<Square>> Squares;
+        private List<Ship> Ships;
+
+        public Ocean(List<Ship> ships)
+        {
+            this.Squares = GenerateSquares(ships);
+            this.Ships = ships;
+        }
+        public void AddShip(int size, bool isvert, int[] startingpoint) {
+            Ship shipToAdd = new Ship(size, isvert, startingpoint);
+            Ships.Add(shipToAdd);
+        }
+        
+        public List<List<Square>> InsertShipsList(){
+            foreach (var ship_ in Ships) {
+                int ship_x = ship_.GetStartingPoint()[0];
+                int ship_y = ship_.GetStartingPoint()[1];
+                if (ship_.IsShipHorizontal()) {
+                    for (int i = 0; i < ship_.Squares.Count; i++) {
+                        Squares[ship_x + i][ship_y] = ship_.Squares[i];
+                    }
+                }
+                else {
+                    for (int i = 0; i < ship_.Squares.Count; i++) {
+                        Squares[ship_x][ship_y + i] = ship_.Squares[i];
+                    }
+                }
+            }
+            return Squares;
+        }
+        protected List<List<Square>> GenerateSquares(List<Ship> ships) {
+            for (int i = 0; i < HEIGHT; i++){
+                List<Square> squareList = new List<Square>();
+                Squares.Add(squareList);
+                for (int j = 0; j < WIDTH; j++) {
+                    Square newSquare = new Square();
+                    Squares[i].Add(newSquare);
+                }
+            }
+            Squares = InsertShipsList();
+            return Squares;
+    
+        }
+    }
+}
