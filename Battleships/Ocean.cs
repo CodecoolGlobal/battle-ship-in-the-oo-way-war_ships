59be11c7d0ea80745b1e6c18450d55d22960b07e
ndrd <debowyen@gmail.com>
2020-06-02 01:50:10 +0200

    Rand generate sips initialized, View started once again. There is need to check <<Interfaces>>

--- Battleships/Ocean.cs
+++ Battleships/Ocean.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BattleShips
 {
@@ -9,53 +10,105 @@ namespace BattleShips
         private static int WIDTH = 10;
         private List<List<Square>> Squares;
         private List<Ship> Ships;
+        private Random randBase = new Random();
 
         public Ocean(List<Ship> ships)
         {
             this.Squares = GenerateSquares(ships);
             this.Ships = ships;
         }
-        public void AddShip(int size, bool isvert, int[] startingpoint) {
+        public void AddShip(int size, bool isvert, int[] startingpoint) 
+        {
             Ship shipToAdd = new Ship(size, isvert, startingpoint);
             Ships.Add(shipToAdd);
         }
         
-        public void InsertShipsList() {
+        public void InsertShipsList() 
+        {
             Ship _ship;
-            for (int j = 0; j < Ships.Count; j++) {
+            for (int j = 0; j < Ships.Count; j++) 
+            {
                 _ship = Ships[j];
                 int ship_x = _ship.GetStartingPoint()[0];
                 int ship_y = _ship.GetStartingPoint()[1];
-                if (_ship.IsShipHorizontal()) {
-                    for (int i = 0; i < _ship.Squares.Count; i++) {
+                if (_ship.IsShipHorizontal()) 
+                {
+                    for (int i = 0; i < _ship.Squares.Count; i++) 
+                    {
                         Squares[ship_y][ship_x + i] = _ship.Squares[i];
                     }
                 }
-                else {
-                    for (int i = 0; i < _ship.Squares.Count; i++) {
+                else 
+                {
+                    for (int i = 0; i < _ship.Squares.Count; i++) 
+                    {
                         Squares[ship_y + 1][ship_x] = _ship.Squares[i];
                     }
                 }
             }
         }
-        protected List<List<Square>> GenerateSquares(List<Ship> ships) {
-            for (int i = 0; i < HEIGHT; i++){
+        protected List<List<Square>> GenerateSquares(List<Ship> ships) 
+        {
+            for (int i = 0; i < HEIGHT; i++)
+            {
                 List<Square> squareList = new List<Square>();
                 Squares.Add(squareList);
-                for (int j = 0; j < WIDTH; j++) {
+                for (int j = 0; j < WIDTH; j++) 
+                {
                     Square newSquare = new Square(" ", false);
                     Squares[i].Add(newSquare);
                 }
             }
+            InsertShipsList();
             return Squares;
         }
-        public List<List<Square>> GetOceanSquares() {
-            return Squares;
-        }
-
-        public List<Ship> GetShips()
+        public List<List<Square>> GetOceanSquares() 
         {
-            return this.Ships;
+            return Squares;
+        }
+        // Getting List<Ship> of ships on Ocean
+        public List<Ship> GetOceanShips()
+        {
+            return Ships;
+        }
+        // Checking coordinates of a shot if they belong to Ship coords format int[2] [Xcoord, Ycoord] ex. A5 = [0,5]
+        public bool IsCoordShip(int[] coord)
+        {
+            int coordX = coord[0];
+            int coordY = coord[1];
+
+            foreach (Ship ship in Ships) 
+            {
+                int shipX = ship.GetStartingPoint()[0];
+                int shipY = ship.GetStartingPoint()[1];
+                var shipXRange = Enumerable.Range(shipX, ship.IsShipHorizontal()? ship.Squares.Count : 1);
+                var shipYRange = Enumerable.Range(shipY, ship.IsShipHorizontal()? 1 : ship.Squares.Count);
+                if (shipXRange.Contains(shipX))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /*Method to create random ship with coditions:
+        - putting ship on board that does not overlaps with other ships - it has to get the coords of all existing Ships in obj field
+        - not only not overlaps but they can not touch - there has to be expanded list of coords out of limits
+        - 50% horizontal
+        - if ship can not be placed with chosen arrangement it flips and tries again with differnt one
+        - raises error when it is not possible
+        */
+        public Ship RandomlyGenerateShip(int size)
+        {
+            var randIsHorizontal = randBase.Next(0,100) < 50 ? true : false;
+            while (true)
+            {
+                break;
+            }
+            return Ships[0];
+        }
+        public List<Ship> RandomlyGenerateShips()
+        {
+            return Ships;
         }
     }
 }
--- Battleships/Ship.cs
+++ Battleships/Ship.cs
@@ -3,31 +3,44 @@ using System.Collections.Generic;
 
 namespace BattleShips
 {
-    public class Ship {
+    public class Ship 
+    {
 
         public List<Square> Squares {get; set;}
         private bool isHorizontal;
         private int[] StartingPoint = new int[2];
 
-        public Ship(int size, bool ishorizontal, int[] startingPoint) {
+        // Important note: coords are [x,y], which means that: Squares[y][x]
+        public Ship(int size, bool ishorizontal, int[] startingPoint) 
+        {
             
             List<Square> squares = new List<Square>();
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) 
+            {
                 squares[i] = new Square("S", false);
             }
             this.Squares = squares;
             this.isHorizontal = ishorizontal;
             this.StartingPoint = startingPoint;
         }
-        public bool IsShipHorizontal() {
+
+        // Getting info about ship being horizontal or not
+        public bool IsShipHorizontal() 
+        {
             return isHorizontal;
         }
-        public int[] GetStartingPoint() {
+        // Getting ships starting point
+        public int[] GetStartingPoint() 
+        {
             return StartingPoint;
         }
-        public bool ShipSank() {
-            foreach (var square in Squares) {
-                if (square.isHit) {
+        // Getting info if ship's sank
+        public bool ShipSank() 
+        {
+            foreach (var square in Squares) 
+            {
+                if (square.isHit) 
+                {
                     return false;
                 }
             }
