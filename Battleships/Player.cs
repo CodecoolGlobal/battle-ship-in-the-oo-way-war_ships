5217caf530a2148b8a5617d419e8fb0fd29a22a3
Dominik <dominikdrazek99@gmail.com>
2020-06-05 00:19:51 +0200

    Added checkIfLoser method

--- Battleships/Player.cs
+++ Battleships/Player.cs
@@ -3,36 +3,45 @@ using System.Collections.Generic;
 using System.Drawing;
 
 
+
 namespace BattleShips
 {
-    class Player
+    public class Player
     {
 
         public Ocean playerOcean;
         private string Name;
         private bool IsComp;
         private bool IsLoser;
+        private Ship playerShip;
+        private List<List<Squares>> playerSquares;
 
         public Player(string name)
         {
             List<Ship> ships = new List<Ship>();
             this.Name = name;
             this.playerOcean = new Ocean(ships); 
+            this.playerShip = new Ship(startingPoint);
+            this.playerSquares = new Ocean(squares);
+            
         }
 
-        public int enemyAttack(char[] coords)
+        public int enemyAttack(Point coords)
         {
-            foreach (var ship in playerOcean.GetOceanShips())
+            foreach (var ship in playerOcean.GetShips())
             {
-                for
+                if (ship
             }
         }
         
         public bool checkIfLoser()
         {
-            foreach (var ship in playerOcean.GetOceanShips())
+            foreach (var squares in playerOcean.GetOceanSquares())
             {
-
+                foreach (var square in squares)
+                {
+                    if 
+                }
             }
         }
 
